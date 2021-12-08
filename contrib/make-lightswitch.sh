61049a9734bf7745170efb410234e6c3bb6d7424
Lee <ler762@users.sourceforge.net>
2021-12-08 16:05:51 -0500

    I'm way too used to developing on windows/cygwin

--- contrib/make-lightswitch.sh
+++ contrib/make-lightswitch.sh
@@ -6,50 +6,46 @@
 #    https://www.github.developerdan.com/hosts/lists/tracking-aggressive-extended.txt
 # de-duplicate, and save as lightswitch-hosts.txt
 
-# SCRIPT="block-test_old.awk"
+umask 000
+
 SCRIPT="block-test.awk"
-
-########################################################################
-if [ "$1" = "time" ]; then
-   # just want a timing run (vs. d/l a new file and creating a new action file)
-   TIMING="yes"
-fi
-
-if [ "$TIMING" != "yes" ]; then
 
 set -x
 
+TD=`mktemp -q -d /tmp/LSXXXXXXXX`
+stat=$?
+if [ $stat -ne 0 ]; then
+  echo "barf: unable to create tmp directory, status=${stat}"
+  exit 1
+fi
+
 # get the new hosts file
-curl -q -sS https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt  > ads-and-tracking-extended.txt
+curl -q -sS https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt  > ${TD}/ads-and-tracking-extended.txt
 stat=$?
 if [ $stat -ne 0 ]; then
    echo "error $stat: curl https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt"
-   exit
+   exit 10
 fi
-curl -q -sS https://www.github.developerdan.com/hosts/lists/tracking-aggressive-extended.txt > tracking-aggressive-extended.txt
+curl -q -sS https://www.github.developerdan.com/hosts/lists/tracking-aggressive-extended.txt > ${TD}/tracking-aggressive-extended.txt
 stat=$?
 if [ $stat -ne 0 ]; then
    echo "error $stat: curl https://www.github.developerdan.com/hosts/lists/tracking-aggressive-extended.txt"
-   exit
+   exit 11
 fi
 
 #  get the title, date, etc. header info from the files
-head -10 ads-and-tracking-extended.txt     > lightswitch-hosts.txt
-head -10 tracking-aggressive-extended.txt >> lightswitch-hosts.txt
+head -10 ${TD}/ads-and-tracking-extended.txt     > ${TD}/lightswitch-hosts.txt
+head -10 ${TD}/tracking-aggressive-extended.txt >> ${TD}/lightswitch-hosts.txt
 
 #  remove leading "0.0.0.0 ", leading/trailing spaces, comments and blank lines
 sed  -e 's/^0\.0\.0\.0 //'  \
      -e 's/^  *//'  \
      -e 's/  *$//'  \
      -e 's/#.*$//'  \
-     -e '/^$/d'  ads-and-tracking-extended.txt  tracking-aggressive-extended.txt |\
-fqdnsort >> lightswitch-hosts.txt
+     -e '/^$/d'  ${TD}/ads-and-tracking-extended.txt  ${TD}/tracking-aggressive-extended.txt |\
+fqdnsort >> ${TD}/lightswitch-hosts.txt
 
-fi  # endif $TIMING != "yes"
-########################################################################
-
-
-# save the original Privoxy config.txt
+# save the original Privoxy config
 cp -p config.txt config-original.txt
 
 # stop using the to-be-updated action file & all others after it
@@ -63,26 +59,27 @@ sed \
 mv config.new config.txt
 
 # get Privoxy to re-read it's config
-curl -q -sS --proxy 127.0.0.1:8118 --referer http://p.p/ http://p.p/show-status > /dev/null
+curl -q -sS --proxy 127.0.0.1:8118 --referer http://config.privoxy.org/ http://config.privoxy.org/show-status > /dev/null
+stat=$?
+if [ $stat -ne 0 ]; then
+   echo "error $stat: curl -q -sS --proxy 127.0.0.1:8118 --referer http://config.privoxy.org/ http://config.privoxy.org/show-status"
+   echo "\'sudo systemctl start privoxy\'  to start Privoxy"
+   exit 20
+fi
 
 # see how long it takes to make the new .action file
-date +%s > timestamp.txt
+date +'%s  %c' > ${TD}/timestamp.txt
 
-echo "{ +block{new lightswitch hosts file} }"> lightswitch-hosts.new
-gawk -f $SCRIPT lightswitch-hosts.txt       >> lightswitch-hosts.new
+echo "{ +block{new lightswitch hosts file} }"> ${TD}/lightswitch-hosts.new
+gawk -f $SCRIPT ${TD}/lightswitch-hosts.txt >> ${TD}/lightswitch-hosts.new
 
-date +%s >> timestamp.txt
+date +'%s  %c' >> ${TD}/timestamp.txt
 
 # restore the original Privoxy config
 mv config-original.txt config.txt
 
 # so how long did it take?
-gawk -f elapsed.awk timestamp.txt
+gawk -f elapsed.awk ${TD}/timestamp.txt
 
-if [ "$TIMING" = "yes" ]; then
-  grep '^  print "GET http://config.privoxy.org' $SCRIPT >> timing.txt
-  gawk -f elapsed.awk timestamp.txt                      >> timing.txt
-else
-  /cygdrive/c/MyProgs/Winmerge/WinmergeU.exe  lightswitch-hosts.new  lightswitch-hosts.action
-fi # endif $TIMING
+meld  lightswitch-hosts.new  /etc/privoxy/lightswitch-hosts.action
 
