392b1ee5243ccd18e12ebfb3932a0bc8e285b877
Lee <ler762@users.sourceforge.net>
2021-12-08 16:07:21 -0500

    developing on windows+cygwin is much less restrictive than debian

--- contrib/make-unified.sh
+++ contrib/make-unified.sh
@@ -6,30 +6,39 @@
 #      (from main page: https://github.com/StevenBlack/hosts)
 # and save it as unified-hosts.txt
 
+umask 000
+
 SCRIPT="block-test.awk"
 
 set -x
 
+TD=`mktemp -q -d /tmp/UNIXXXXXXX`
+stat=$?
+if [ $stat -ne 0 ]; then
+  echo "barf: unable to create tmp directory, status=${stat}"
+  exit 1
+fi
+
 # get the new hosts file
-curl -q -sS https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts > unified-hosts.txt
+curl -q -sS https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts > ${TD}/unified-hosts.txt
 stat=$?
 if [ $stat -ne 0 ]; then
    echo "error $stat: curl https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
-   exit
+   exit 10
 fi
 
 #  get the title, date, etc. header info from the file
-head -14 unified-hosts.txt > unified-hosts.srt
+head -14 ${TD}/unified-hosts.txt > ${TD}/unified-hosts.srt
 
 #  remove leading "0.0.0.0 ", leading/trailing spaces and blank lines
 sed  -e 's/^0\.0\.0\.0 //'  \
      -e 's/^  *//'  \
      -e 's/  *$//'  \
-     -e '/^$/d'    unified-hosts.txt |\
-  fqdnsort      >> unified-hosts.srt
+     -e '/^$/d'    ${TD}/unified-hosts.txt |\
+  fqdnsort      >> ${TD}/unified-hosts.srt
 
 
-# save the original Privoxy config.txt
+# save the original Privoxy config
 cp -p config.txt config-original.txt
 
 # stop using the to-be-updated action file & all others after it
@@ -42,21 +51,27 @@ sed \
 mv config.new config.txt
 
 # get Privoxy to re-read it's config
-curl -q -sS --proxy 127.0.0.1:8118   http://config.privoxy.org/ > /dev/null
+curl -q -sS --proxy 127.0.0.1:8118  http://config.privoxy.org/show-status > /dev/null
+stat=$?
+if [ $stat -ne 0 ]; then
+   echo "error $stat: curl -q -sS --proxy 127.0.0.1:8118  http://config.privoxy.org/show-status"
+   echo "\'sudo systemctl start privoxy\'  to start Privoxy"
+   exit 20
+fi
 
 # see how long it takes to make the new .action file
-date +%s > timestamp.txt
+date +'%s  %c' > ${TD}/timestamp.txt
 
-echo "{ +block{new unified hosts file} }"> unified-hosts.new
-gawk -f $SCRIPT unified-hosts.srt       >> unified-hosts.new
+echo "{ +block{new unified hosts file} }"> ${TD}/unified-hosts.new
+gawk -f $SCRIPT ${TD}/unified-hosts.srt >> ${TD}/unified-hosts.new
 
-date +%s >> timestamp.txt
+date +'%s  %c' >> ${TD}/timestamp.txt
 
 # restore the original Privoxy config
 mv config-original.txt config.txt
 
 # so how long did it take?
-gawk -f elapsed.awk timestamp.txt
+gawk -f elapsed.awk ${TD}/timestamp.txt
 
-/cygdrive/c/MyProgs/Winmerge/WinmergeU.exe  unified-hosts.new  unified-hosts.action
+meld  unified-hosts.new  /etc/privoxy/unified-hosts.action
 
