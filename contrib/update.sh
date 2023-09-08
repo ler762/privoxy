9a0ef3f210d549a6c038f4383ccc0fe00d530171
Lee <ler762@users.sourceforge.net>
2023-09-08 13:06:26 -0400

    bump timstamp - will they upload now?

--- contrib/update.sh
+++ contrib/update.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 ./make-pgl.sh
-./make-unified.sh
-./make-lightswitch.sh
-./make-1hosts.sh
+printf "\n\n\n"
 
+./make-unified.sh
+printf "\n\n\n"
+
+./make-lightswitch.sh
+printf "\n\n\n"
+
+./make-1hosts.sh
+printf "\n\n\n"
+
+echo "finished $(date)"
